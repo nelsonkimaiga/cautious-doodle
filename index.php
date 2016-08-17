de5a10957d738ef03a95a369b8c582b433a4d4bc
kimaiga <nelsonkimaiga@aol.com>
2016-08-17 16:49:15 +0300

    Design of landing page

--- index.php
+++ index.php
@@ -28,17 +28,59 @@
         <!--foundation-->
         <link rel="stylesheet" type="text/css" href="css/foundation.css">
         <link rel="stylesheet" type="text/css" href="css/app.css">
+        <!--fonts-->
+        <link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
+        <!--font-awsome-->
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+        <!--foundation icons-->
     </head>
     <body>
-        <div>
-            <?php
-            echo 'Mjamaa';
-            ?>
+        <nav class="nav ">
+            <div class="nav-center">
+                <a class="toggle-nav" href="#">&#9776;</a>
+                <div class="nav-mobile">
+                    <ul class="left">
+                        <li><a href="">Home</a></li>
+                        <li><a href="">Vacancies</a></li>
+                    </ul>
+                    <ul class="right">
+                        <li>
+                            <a href="#">Employer Login/Register</a>
+                        </li>
+                        <li>
+                            <a href="#">Applicant Login/Register</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <div class="container bodycontent">
+            <div class="row">
+                <h4>The Rhapsody Hiring Portal</h4>
+                <p>Keep Track of your job applications</p>
+                <hr>
+                <div class="large-6 columns">
+                    <form action="index.php" method="post" name="index" id="index" enctype="multipart/form-data">
+                        <fieldset>
+                            <label>Job Keyword</label>
+                            <input type="text" name="keyboard">
+                        </fieldset>
+                        <fieldset>
+                            <select name="countryid" id="countryid">
+                                <option value="default" selected disabled>select country</option>
+                            </select>
+                        </fieldset>
+                        <button class="button success" type="submit" name="submit">Search</button>
+                        <a href="">Advanced Search</a>
+                    </form>
+                </div>
+                
+            </div>
         </div>
         
         <script src="js/vendor/jquery.js"></script>
         <script src="js/foundation.js"></script>
         <script src="js/app.js"></script>
-        <script src="js/waypoint.js"></script>
+        <script src="js/what-input.js"></script>
     </body>
 </html>
