6231d0b0170904a401a8a6399dc8b415f81a01ac
kimaiga <nelsonkimaiga@aol.com>
2016-08-17 14:57:15 +0300

    added project structure

--- index.php
+++ index.php
@@ -1,18 +1,25 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
+<html class="no-js" lang="en" dir="ltr">
     <head>
+        <title>Cautious Doodle</title>
         <meta charset="UTF-8">
-        <title></title>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link type="text/css" href="css/foundation.css" rel="stylesheet">
+        <link type="text/css" href="css/app.css" rel="stylesheet">
+        <!--fonts-->
+        <link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
     </head>
     <body>
-        <?php
-        // put your code here
-        echo "Aye!!";
-        ?>
+        <div class="container">
+            <h2>
+                <?php
+                echo "Fala wewe"
+                ?>
+            </h2>
+        </div>
+        <script src="js/vendor/jquery.js"></script>
+        <script src="js/vendor/foundation.js"></script>
+        <script src="js/app.js"></script>
     </body>
 </html>
